1833a188ed636c795f526b4ecffb34a96ea19ae5
isha zaka <ishazaka@ishas-MacBook-Air.local>
2023-05-25 07:49:04 -0400

    staring marina UI

--- src/App.js
+++ src/App.js
@@ -5,18 +5,10 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       <h1>Marina</h1>
+       <h3>A call center for Startup to help founder interect with their users</h3>
+       
+        
       </header>
     </div>
   );
